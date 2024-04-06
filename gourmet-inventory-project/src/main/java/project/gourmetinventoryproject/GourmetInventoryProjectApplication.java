79ad75ef9221be508193e9b7beaf8f24367ed362
AikoJu <julya.aono@sptech.school>
2024-04-06 14:16:15 -0300

    main

---
+++ gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/GourmetInventoryProjectApplication.java
@@ -0,0 +1,13 @@
+package project.gourmetinventoryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetInventoryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetInventoryProjectApplication.class, args);
+	}
+
+}
---
+++ gourmet-inventory-project/src/test/java/project/gourmetinventoryproject/GourmetInventoryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.gourmetinventoryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetInventoryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
