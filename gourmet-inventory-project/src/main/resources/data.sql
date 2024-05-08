47c88a29d11026a43e26e4013256a2d7d143d2ff
voltty <maranhopeterson@gmail.com>
2024-05-08 02:43:41 -0300

    atualizando dependencias, application.properties e data.sql

---
+++ gourmet-inventory-project/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuarios (nome, cargo, cpf, email, celular, senha)VALUES ('John Doe','administrador','123.456.678-90','john@doe.com', '11954563749','$2a$10$0/TKTGxdREbWaWjWYhwf6e9P1fPOAMMNqEnZgOG95jnSkHSfkkIrC');
+INSERT INTO usuarios (nome, cargo, cpf, email, celular, senha)VALUES ('Julya', 'administrador', '987.654.321-09', 'julya@example.com', '11987654321', '$2a$10$0/TKTGxdREbWaWjWYhwf6e9P1fPOAMMNqEnZgOG95jnSkHSfkkIrC');
+INSERT INTO usuarios (nome, cargo, cpf, email, celular, senha)VALUES ('peterson', 'administrador', '987.654.321-09', 'peterson@example.com', '11987654321', '$2a$10$0/TKTGxdREbWaWjWYhwf6e9P1fPOAMMNqEnZgOG95jnSkHSfkkIrC');
+INSERT INTO usuarios (nome, cargo, cpf, email, celular, senha)VALUES ('luan', 'administrador', '456.789.123-45', 'luan@example.com', '11912345678', '$2a$10$0/TKTGxdREbWaWjWYhwf6e9P1fPOAMMNqEnZgOG95jnSkHSfkkIrC');
+INSERT INTO usuarios (nome, cargo, cpf, email, celular, senha)VALUES ('mirella', 'administrador', '789.123.456-78', 'mirella@example.com', '11998765432', '$2a$10$0/TKTGxdREbWaWjWYhwf6e9P1fPOAMMNqEnZgOG95jnSkHSfkkIrC');
+INSERT INTO usuarios (nome, cargo, cpf, email, celular, senha)VALUES ('kevin', 'administrador', '321.654.987-01', 'kevin@example.com', '11945678901', '$2a$10$0/TKTGxdREbWaWjWYhwf6e9P1fPOAMMNqEnZgOG95jnSkHSfkkIrC');
+
+INSERT INTO empresas VALUES (1,'Empresa A', '12345678000101', '1122334455', 1);
+INSERT INTO empresas VALUES (2,'Empresa B', '23456789000121', '9988776655', 2);
+INSERT INTO empresas VALUES (3,'Empresa C', '34567890000132', '5544332211', 3);
+INSERT INTO empresas VALUES (4,'Empresa D', '45678901200143', '6677889900', 4);
