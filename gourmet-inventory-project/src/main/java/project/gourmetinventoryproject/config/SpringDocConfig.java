a3a68afe253cf516296e75521a0e7b7544670c57
voltty <maranhopeterson@gmail.com>
2024-05-08 01:27:10 -0300

    autenticação JWT pelo swagger!!!

--- gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/config/SpringDocConfig.java
+++ gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/config/SpringDocConfig.java
@@ -1,6 +1,9 @@
 package project.gourmetinventoryproject.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +19,11 @@ public class SpringDocConfig {
                        .description("API para gerenciamento de estoque de ingredientes e pratos")
                        .contact(new io.swagger.v3.oas.models.info.Contact()
                                .email("gourmet_inventory@outlook.com")
-                               .name("Equipe de Desenvolvimento")));
+                               .name("Equipe de Desenvolvimento"))).components(new Components()
+                .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
     }
 }
