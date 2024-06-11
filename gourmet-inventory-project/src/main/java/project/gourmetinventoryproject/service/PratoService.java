2f4265e9c2dc0fd7a6195fb8c8774cf4d9a85834
Luan Ribeiro <luan.magno@sptech.school>
2024-06-11 18:22:56 -0300

    função updatePratoFoto

--- gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/service/PratoService.java
+++ gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/service/PratoService.java
@@ -1,7 +1,11 @@
 package project.gourmetinventoryproject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import project.gourmetinventoryproject.domain.Prato;
 import project.gourmetinventoryproject.domain.Receita;
 import project.gourmetinventoryproject.repository.PratoRepository;
@@ -16,6 +20,8 @@ import java.util.HashMap;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.http.ResponseEntity.status;
 
 @Service
 public class PratoService {
@@ -73,15 +79,65 @@ public class PratoService {
     }
 
     public Map<Long, Integer> calculateIngredientUsage(List<Long> servedDishesIds) {
+        // Cria um mapa para armazenar o uso de ingredientes.
+        // Chave: ID do ingrediente (Long)
+        // Valor: Quantidade do ingrediente usado (Integer)
         Map<Long, Integer> ingredientUsage = new HashMap<>();
 
+        // Itera sobre todos os IDs de pratos servidos fornecidos como entrada para o método.
         for (Long dishId : servedDishesIds) {
+            // Obtém a lista de receitas associadas ao prato atual usando o ID do prato.
+            // Supõe-se que receitaRepository.findByIdPrato(dishId) retorne uma lista de objetos do tipo Receita.
             List<Receita> recipes = receitaRepository.findByIdPrato(dishId);
+
+            // Itera sobre todas as receitas associadas ao prato atual.
             for (Receita recipe : recipes) {
-                ingredientUsage.put(recipe.getIdIngrediente(), ingredientUsage.getOrDefault(recipe.getIdIngrediente(), 0) + recipe.getQuantidade());
+                // Atualiza o mapa ingredientUsage:
+                // Se o ingrediente já estiver presente no mapa, o valor existente é substituído pela soma do valor existente e da quantidade do ingrediente na receita atual.
+                // Se o ingrediente não estiver presente, ele é adicionado ao mapa com a quantidade da receita atual.
+                ingredientUsage.merge(recipe.getIdIngrediente(), recipe.getQuantidade(), Integer::sum);
             }
         }
 
+        // Retorna ingredientUsage, que contém o uso de ingredientes calculado para todos os pratos servidos.
         return ingredientUsage;
+    }
+
+
+    public int[][] generateIngredientUsageReport(List<Long> servedDishesIds, int numberOfIngredients) {
+        int[][] ingredientUsageReport = new int[servedDishesIds.size()][numberOfIngredients];
+
+        for (int i = 0; i < servedDishesIds.size(); i++) {
+            Long dishId = servedDishesIds.get(i);
+            List<Receita> recipes = receitaRepository.findByIdPrato(dishId);
+
+            for (Receita recipe : recipes) {
+                int ingredientIndex = recipe.getIdIngrediente().intValue() - 1;
+                int quantity = recipe.getQuantidade();
+                ingredientUsageReport[i][ingredientIndex] += quantity;
+            }
+        }
+
+        return ingredientUsageReport;
+
+        /*Resultado esperado
+                    | Ingrediente 1 | Ingrediente 2 | Ingrediente 3 | Ingrediente 4 |
+            ----------------------------------------------------------------------
+            Prato 1 |       1       |       3       |       0       |       1       |
+            Prato 2 |       0       |       4       |       2       |       0       |
+            Prato 3 |       3       |       0       |       8       |       7       |
+        */
+    }
+
+    public Prato updatePratoFoto(@PathVariable Long codigo, @RequestBody byte[] novaFoto) {
+        Prato prato;
+        if (pratoRepository.existsById(codigo) == false){
+            throw new IdNotFoundException();
+        } else {
+            prato = pratoRepository.findById(codigo).get();
+            prato.setFoto(novaFoto);
+            pratoRepository.save(prato);
+        }
+        return prato;
     }
 }
