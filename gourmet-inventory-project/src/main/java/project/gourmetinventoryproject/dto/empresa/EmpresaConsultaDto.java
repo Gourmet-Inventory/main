b7c173f56eb697a9b1566831bf2c7b5f63e4d417
AikoJu <julya.aono@sptech.school>
2024-06-13 21:45:24 -0300

    Ajuste de empresa para post no front

---
+++ gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/dto/empresa/EmpresaConsultaDto.java
@@ -0,0 +1,13 @@
+package project.gourmetinventoryproject.dto.empresa;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EmpresaConsultaDto {
+    private String nomeFantasia;
+    private String cnpj;
+    private String telefone;
+    private Long responsavelId;
+}
--- gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/dto/usuario/UsuarioCriacaoDto.java
+++ gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/dto/usuario/UsuarioCriacaoDto.java
@@ -1,5 +1,6 @@
 package project.gourmetinventoryproject.dto.usuario;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -13,20 +14,18 @@ import org.hibernate.validator.constraints.br.CPF;
 @NoArgsConstructor
 @Data
 public class UsuarioCriacaoDto {
-
-    @NotBlank
+    @NotBlank(message = "Preencha o campo!")
     private String nome;
     @NotBlank
     private String cargo;
-    @CPF
-    @NotBlank
-    private String cpf;
     @Email
-    @NotBlank
+    @NotBlank(message = "Preencha o campo!")
     private String email;
+    @NotBlank(message = "Preencha o campo!")
+    @Pattern(regexp = "^\\(\\d{2}\\) (?:9\\d{4}|\\d{4})-\\d{4}$",message = "Telefone inválido")
     private String celular;
     @Size(min = 6)
-    //@Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])")//senha forte | Pelo menos uma letra minúscula | Pelo menos uma letra maiúscula | Pelo menos um número | Pelo menos um caractere especial
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])")
     @NotBlank
     private String senha;
     private Long idEmpresa;
