6c2051ab5129787ce0dd82d3f4e7c64912df1377
KevinDSWesselka <kevin.wesselka@sptech.school>
2024-10-28 14:51:23 -0300

    Add: Comanda

---
+++ gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/repository/ComandaRepository.java
@@ -0,0 +1,2 @@
+package project.gourmetinventoryproject.repository;public interface ComandaRepository {
+}
---
+++ gourmet-inventory-project/src/main/java/project/gourmetinventoryproject/service/ComandaService.java
@@ -0,0 +1,2 @@
+package project.gourmetinventoryproject.service;public class ComandaService {
+}
